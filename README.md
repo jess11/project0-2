cc0c92a08d14ec6602549eb99765f6cfecca1edc
Jessica Tran <kryx11@gmail.com>
2016-12-21 15:36:16 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+***** MONKEYS AND UNICORNS ****
+----------------------------------
+
+DESCRIPTION
+----------------------------
+Play noughts and crosses but as a monkey and a unicorn!! ^_^ ~~
+
+
+
+HOW TO PLAY
+---------------
+- Choose the board size first by inputting a number between 3-20 and press the OK button.
+- Two player game. Players choose either to be a monkey or a unicorn. The monkey starts first.
+- Click on the grid and put your marker down where you want.
+- Players alternate in turns.
+- To win, get your markers in a row, either vertical, horizontal or diagonal.
+- Scores will be added onto the score board at the end of each game. To play another game click on the "Play Again" button.
+- If you want to reset the scores, click on the "Reset scores" button.
+
+
+LOGIC
+----------------
+- Structured in a table in a HTML document.
+- If a cell is clicked on, a marker is placed into the table cell (by adding an image to the table cell) depending on which player's turn it is.
+- The cell will also be given a "clicked" class so that it cannot be clicked on more than once
+- To find out who won, pass through the ID names of the cells to see if there are any winning combinations. If a combination was found, then a winner will be called.
+- To reset the board, all classes are removed from the table cells and the values of the global variables are reset.
+
+
+CONTACT
+----------------
+Jess: kryx11@gmail.com
---
+++ js/tictactoe.js
@@ -0,0 +1,178 @@
+// $(document).ready(function(){
+
+//making the table- THIS MUST COME FIRST- put in prompt
+
+var tableSize = 0;
+
+
+var player = 0;
+var moves = 0;
+var inputX =[];
+var inputY = [];
+var scoreX = 0;
+var scoreY = 0;
+
+//Find if there is a win
+
+winnings = function (input){
+  //Horizontal wins
+  for (var i = 0 ; i <tableSize ; i++ ){
+    var count=0;
+    for (var j = 0 ; j <input.length ; j++ ){
+      if(parseInt(input[j].slice(1,2)) === i ){
+        count +=1;
+      }
+      if (count === tableSize){
+        return true
+      }
+    }
+    count=0;
+  }
+
+  //Vertical wins
+  for (var i = 0 ; i <tableSize ; i++ ){
+    var count=0;
+    for (var j = 0 ; j <input.length ; j++ ){
+      if(parseInt(input[j].slice(2)) === i ){
+        count +=1;
+      }
+      if (count === tableSize){
+        return true
+      }
+    }
+    count=0;
+  }
+
+  //Diagonal wins
+  var diagonal1 = [];
+  var diagonal2 =[];
+  for (var i = 0; i<tableSize; i++ ){
+    var y = "c" + i.toString() + i.toString();
+    diagonal1.push(y);
+    var x = "c" + (tableSize-1-i).toString() + i.toString();
+    diagonal2.push(x);
+  }
+
+  var count1 = 0;
+  for(var i =0; i < tableSize ; i++){
+    if($.inArray(diagonal1[i], input) > -1){
+      count1 +=1;
+      if(count1 === tableSize){
+        return true;
+      }
+    }
+  }
+
+  var count2 = 0;
+  for(var i =0; i < tableSize ; i++){
+    if($.inArray(diagonal2[i], input) > -1){
+      count2 +=1;
+      if(count2 === tableSize){
+        return true;
+      }
+    }
+  }
+}
+
+//blocking any further clicking
+var blockClicking = function (){
+  for (var i = 0; i<$('td').length; i++){
+    $($('td')[i]).addClass('clicked');
+  }
+}
+
+//reset board (via button)
+var reset = function (){
+  for (var i = 0; i<$('td').length; i++){
+    $($('td')[i]).removeClass();
+    $($('td')[i]).html('');
+  }
+  player = 0;
+  moves = 0;
+  inputX =[];
+  inputY = [];
+  $('#winner').html('')
+
+}
+
+//prints score to the screen
+var score = function(){
+  $('#scoreX').html('Monkey: ' + scoreX );
+  $('#scoreY').html('Unicorn: ' + scoreY );
+}
+
+//reset scores (via button)
+var resetScore = function(){
+  scoreX = 0;
+  scoreY = 0;
+  $('#winner').html('');
+  score();
+}
+
+
+//////////START OF TICTACTOE ////////////////////////////
+
+var tictactoe = function(){
+  //putting a marker into the gridSize, appointing which player's turn and adding a class to prevent clicking on the box more than once.
+  if($(this).hasClass('clicked')){
+    return false
+  }
+  if(player === 0){
+    $(this).html('<img src="images/banana.png" id="banana">');
+    player +=1;
+    inputX.push($(this).attr('id'));
+  } else {
+    $(this).html('<img src="images/unicorn.jpeg" id="unicorn">');
+    player -=1;
+    inputY.push($(this).attr('id'));
+  }
+
+  $(this).addClass('clicked');
+
+  if (winnings(inputX) || winnings(inputY)) {
+    blockClicking();
+    if ((player-1) === 0){
+      scoreX += 1;
+      $('#winner').html('Monkey wins!');
+    } else {
+      scoreY += 1;
+      $('#winner').html('Unicorn wins!');
+    }
+      score();
+  };
+}
+
+//////////END OF TICTACTOE ////////////////////////////
+// });
+
+var gridSize = function(num){
+  if (num < 3 || num > 20){
+    return false;
+  }
+  var html='';
+
+  for( var i = 0; i<num ; i++ ){
+      html += "<tr>"
+    for (var j = 0; j<num ; j++ ){
+      html += "<td id='c" + i.toString() + j.toString() + "'></td>";
+    }
+    html+="</tr>"
+  }
+  $('table').html(html);
+
+  reset();
+  resetScore();
+  $('td').on('click', tictactoe);
+
+}
+
+var whatSize= function (){
+  tableSize= parseInt($('input').val());
+  gridSize(tableSize);
+}
+
+/////////////////////
+
+$('#enterSize').on('click', whatSize);
+$('.reset').on('click',reset);
+$('.resetScore').on('click',resetScore);
